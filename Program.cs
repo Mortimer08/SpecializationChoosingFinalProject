6da0912166fca1b5a68e52212e44597b245ffff4
Ilya Makarov <mortimer08@gmail.com>
2022-12-03 21:03:48 +0500

    Method ShorterStringsArrayCreate Created

--- Program.cs
+++ Program.cs
@@ -9,22 +9,33 @@
     return arrayOfStings;
 }
 
-int ShortStringsCount(string[] array, int miniLenght)
+int ShortStringsCount(string[] array, int miniLength)
 {
     int shortStringsCount = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length<=miniLenght) shortStringsCount++;
+        if (array[i].Length <= miniLength) shortStringsCount++;
     }
     return shortStringsCount;
 }
 
+string[] ShorterStringsArrayCreate(string[] baseArray, int targetArrayLength, int miniLength)
+{
+    string[] targetArray = new string[targetArrayLength];
+    int j = 0;
+    for (int i = 0; i < baseArray.Length; i++)
+    {
+        if (baseArray[i].Length <= miniLength) targetArray[j++] = baseArray[i];
+    }
+    return targetArray;
+}
+
 Console.Write("Введите количество элементов массива: ");
 int nStrings = Convert.ToInt32(Console.ReadLine());
-
 string[] stringsArray = ArrayInput(nStrings);
+
 int minStringLenght = 3;
+int shorterStings = ShortStringsCount(stringsArray, minStringLenght);
 
-int shorterStings = ShortStringsCount(stringsArray,minStringLenght);
-
+string[]shorterStringsArray = ShorterStringsArrayCreate(stringsArray,shorterStings,minStringLenght);
 Console.WriteLine(shorterStings);
