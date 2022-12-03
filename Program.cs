e26454117d06ffa4393cee65b00727121b8d6221
Ilya Makarov <mortimer08@gmail.com>
2022-12-03 20:42:26 +0500

    Method ArrayInput Created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] ArrayInput(int numberOfElements)
+{
+    string[] arrayOfStings = new string[numberOfElements];
+    for (int i = 0; i < numberOfElements; i++)
+    {
+        Console.Write($"Введите строку № {i + 1}: ");
+        arrayOfStings[i] = Console.ReadLine();
+    }
+    return arrayOfStings;
+}
+
+
+
+Console.Write("Введите количество элементов массива: ");
+int nStrings = Convert.ToInt32(Console.ReadLine());
+
+string []stringsArray = ArrayInput(nStrings);
