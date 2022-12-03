5e7167ea5ac2dabe5df6ac3e7ff54220bff36379
Ilya Makarov <mortimer08@gmail.com>
2022-12-03 20:52:26 +0500

    Method SortStringsCount Created

--- Program.cs
+++ Program.cs
@@ -9,9 +9,22 @@
     return arrayOfStings;
 }
 
-
+int ShortStringsCount(string[] array, int miniLenght)
+{
+    int shortStringsCount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length<=miniLenght) shortStringsCount++;
+    }
+    return shortStringsCount;
+}
 
 Console.Write("Введите количество элементов массива: ");
 int nStrings = Convert.ToInt32(Console.ReadLine());
 
-string []stringsArray = ArrayInput(nStrings);
+string[] stringsArray = ArrayInput(nStrings);
+int minStringLenght = 3;
+
+int shorterStings = ShortStringsCount(stringsArray,minStringLenght);
+
+Console.WriteLine(shorterStings);
